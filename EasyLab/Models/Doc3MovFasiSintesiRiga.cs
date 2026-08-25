961ab78880c3a13db4f3fc66b0c7ca4fc813d9b1
FraBus04 <francescobustreo@proton.me>
2026-08-25 16:52:50 +0200

    Aggiunto dettaglio FasiCommesse

---
+++ EasyLab/Models/Doc3MovFasiSintesiRiga.cs
@@ -0,0 +1,89 @@
+using System;
+
+namespace EasyLab.Models;
+
+public partial class Doc3MovFasiSintesiRiga
+{
+    public int? IdRiga { get; set; }
+
+    public int? IdContatto { get; set; }
+
+    public int? IdCausale { get; set; }
+
+    public int? IdStag { get; set; }
+
+    public int? IdDoc { get; set; }
+
+    public int? IdComm { get; set; }
+
+    public int? IdCCli { get; set; }
+
+    public int? IdFase { get; set; }
+
+    public int? IdMov { get; set; }
+
+    public string? MovCod { get; set; }
+
+    public string? FaseCod { get; set; }
+
+    public string? FaseDesc { get; set; }
+
+    public DateOnly? DataMov { get; set; }
+
+    public string? CdAll { get; set; }
+
+    public string? CdAllCol { get; set; }
+
+    public string? Parte { get; set; }
+
+    public string? ParteDesc { get; set; }
+
+    public string? ParteCol { get; set; }
+
+    public string? ParteColDesc { get; set; }
+
+    public int? TotCapiC { get; set; }
+
+    public int? TotCapiR { get; set; }
+
+    public int? TotCapiDiff { get; set; }
+
+    public bool? CkSaldo { get; set; }
+
+    public string? NrDoc { get; set; }
+
+    public DateOnly? DataDoc { get; set; }
+
+    public DateOnly? DataRicRientro { get; set; }
+
+    public DateOnly? DataConfRientro { get; set; }
+
+    public bool? CkConfRientro { get; set; }
+
+    public string? Nota { get; set; }
+
+    public string? Anag1 { get; set; }
+
+    public int? OrdFase { get; set; }
+
+    public string? CdValuta { get; set; }
+
+    public double? PzArt { get; set; }
+
+    public int? Nr1 { get; set; }
+    public int? Nr2 { get; set; }
+    public int? Nr3 { get; set; }
+    public int? Nr4 { get; set; }
+    public int? Nr5 { get; set; }
+    public int? Nr6 { get; set; }
+    public int? Nr7 { get; set; }
+    public int? Nr8 { get; set; }
+    public int? Nr9 { get; set; }
+    public int? Nr10 { get; set; }
+    public int? Nr11 { get; set; }
+    public int? Nr12 { get; set; }
+    public int? Nr13 { get; set; }
+    public int? Nr14 { get; set; }
+    public int? Nr15 { get; set; }
+    public int? Nr16 { get; set; }
+}
